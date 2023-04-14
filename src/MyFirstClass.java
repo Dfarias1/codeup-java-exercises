e176e11a5e5b23dbb96b75d26a7a0c985d21248a
Daniel <daniel.h.farias2@gmail.com>
2023-04-14 14:40:23 -0500

    hello world

---
+++ src/MyFirstClass.java
@@ -0,0 +1,10 @@
+public class MyFirstClass {
+// shortcut: psvm + tab or main tab to get a main method
+    public static void main(String[] args) {
+        // this will log stuff in your command line
+        // strings need to be in "" (single '' are reserved for single characters)
+        // you must put a ; after every statement or else you will get an syntax error
+        //shortcut is sout + tab
+        System.out.println("Hello, Welcome to Java!");
+    }
+}

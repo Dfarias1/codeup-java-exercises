253dcfc835a650796836bb986a92b669f646ef6a
Daniel <daniel.h.farias2@gmail.com>
2023-04-19 09:18:33 -0500

    method exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,25 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+    }
+        public static int addition(int num1, int num2) {
+            int result = num1 + num2;
+            return result;
+        }
+        public static int subtraction(int num1, int num2) {
+            int result = num1 - num2;
+            return result;
+        }
+        public static int multiplication(int num1, int num2) {
+            int result = num1 * num2;
+            return result;
+        }
+        public static int division(int num1, int num2) {
+            int result = num1 / num2;
+            return result;
+        }
+         public static int modulus(int num1, int num2) {
+            int result = num1 % num2;
+            return result;
+         }
+    }

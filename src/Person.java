4427f06e5ada0cc3decd36479a02cd330e7995e5
Daniel <daniel.h.farias2@gmail.com>
2023-04-20 10:51:09 -0500

    completed person

--- src/Person.java
+++ src/Person.java
@@ -8,38 +8,55 @@ class Person {
     }
 
 
-    public String name;
+//    public String name;
 
-    public Person(String name) {
-        this.name = name;
-    }
+//    public Person(String name) {
+//        this.name = name;
+//    }
 
-    public void sayHello() {
-//        System.out.println("Hello from " + name + "!");
-        System.out.println("Hello, world!");
-    }
-    private String name;
+//    public void sayHello() {
+////        System.out.println("Hello from " + name + "!");
+//        System.out.println("Hello, world!");
+//    }
+//    private String name;
+//
+//    public Person(){
+//    }
+//
+//    public Person(String name){
+//        this.name = name;
+//    }
+//
+//
+//    public String getName(){
+//     return this.name;
+//    }
+//
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//    public void sayHello(){
+//        System.out.println("Hello" + this.name);
+//    }
 
-    public Person(){
-    }
+    public static void main(String[] args){
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));// true because John and John are the same
+//        System.out.println(person1 == person2); // false, b/c theyre two  different instances
 
-    public Person(String name){
-        this.name = name;
-    }
+//        Person person1 = new Person("John");
+//        Person person2 = person1; // assigning the same instance
+//        person1.setName("Wilmarie");
+//        System.out.println(person1 == person2);
 
-
-    public String getName(){
-     return this.name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-    public void sayHello(){
-        System.out.println("Hello" + this.name);
-    }
-
-
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Longer name");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
 
 
     }

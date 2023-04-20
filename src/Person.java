23f8d577f64cdfd8808f0dd0fedd3527c72c8e18
Daniel <daniel.h.farias2@gmail.com>
2023-04-20 14:42:11 -0500

    update

--- src/Person.java
+++ src/Person.java
@@ -3,19 +3,19 @@ class Person {
     public Person(String name) {
         this.name = name;
     }
-    public String getName() {
-        return name;
-    }
-
-
+//    public String getName() {
+//        return name;
+//    }
+//
+//
 //    public String name;
-
+//
 //    public Person(String name) {
 //        this.name = name;
 //    }
-
+//
 //    public void sayHello() {
-////        System.out.println("Hello from " + name + "!");
+//        System.out.println("Hello from " + name + "!");
 //        System.out.println("Hello, world!");
 //    }
 //    private String name;
@@ -46,12 +46,12 @@ class Person {
 //        Person person2 = new Person("John");
 //        System.out.println(person1.getName().equals(person2.getName()));// true because John and John are the same
 //        System.out.println(person1 == person2); // false, b/c theyre two  different instances
-
+//
 //        Person person1 = new Person("John");
 //        Person person2 = person1; // assigning the same instance
 //        person1.setName("Wilmarie");
 //        System.out.println(person1 == person2);
-
+//
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
 //        System.out.println(person1.getName());

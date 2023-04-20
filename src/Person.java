648350812d92ce2f29b1ff296d1f3a4f3606a00d
Daniel <daniel.h.farias2@gmail.com>
2023-04-20 10:50:12 -0500

    person

---
+++ src/Person.java
@@ -0,0 +1,47 @@
+class Person {
+    private String name;
+    public Person(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+//        System.out.println("Hello from " + name + "!");
+        System.out.println("Hello, world!");
+    }
+    private String name;
+
+    public Person(){
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+
+    public String getName(){
+     return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.println("Hello" + this.name);
+    }
+
+
+
+
+    }
+
+}
